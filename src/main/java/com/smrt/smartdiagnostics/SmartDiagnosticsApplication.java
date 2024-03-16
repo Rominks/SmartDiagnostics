ddc8e2d9b6c4afe7528e7a4276aeaaf4f5714ea4
Rominks <galian2002@gmail.com>
2024-03-16 10:09:33 +0200

    init

---
+++ src/main/java/com/smrt/smartdiagnostics/SmartDiagnosticsApplication.java
@@ -0,0 +1,13 @@
+package com.***REMOVED***.smartdiagnostics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDiagnosticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDiagnosticsApplication.class, args);
+	}
+
+}
