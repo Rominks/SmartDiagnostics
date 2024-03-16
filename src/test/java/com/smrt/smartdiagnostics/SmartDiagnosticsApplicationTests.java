21af892fde8049ccb5f2be7ba4c8a0f8c8bf112d
Rominks <galian2002@gmail.com>
2024-03-16 10:09:33 +0200

    init

---
+++ src/test/java/com/smrt/smartdiagnostics/SmartDiagnosticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smrt.smartdiagnostics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDiagnosticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
