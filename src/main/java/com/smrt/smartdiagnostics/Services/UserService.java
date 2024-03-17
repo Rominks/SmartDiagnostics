94ee20760b14d13786678c9fbc35662997aeaa1b
Rominks <galian2002@gmail.com>
2024-03-17 17:32:43 +0200

    Added Registration confirmation endpoint

--- src/main/java/com/smrt/smartdiagnostics/Services/UserService.java
+++ src/main/java/com/smrt/smartdiagnostics/Services/UserService.java
@@ -1,6 +1,8 @@
 package com.smrt.smartdiagnostics.Services;
 
 import com.smrt.smartdiagnostics.Repositories.UserRepository;
+import com.smrt.smartdiagnostics.Users.User;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +13,19 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void createUser(User user) {
+        // Grožis
+        if (!userRepository.existsUserByEmail(user.getEmail())) {
+            if (!userRepository.existsUserByPassword(user.getPassword())) {
+                userRepository.save(user);
+            } else {
+                throw new IllegalStateException("An account exists with the provided password. :)");
+            }
+        } else {
+            throw new IllegalStateException("An account exists with the provided email.");
+        }
     }
 }
