616c2139be7c6a5eee16a3450e3ff3d75f66054f
Rominks <galian2002@gmail.com>
2024-03-17 16:02:25 +0200

    Added user class

---
+++ src/main/java/com/smrt/smartdiagnostics/Users/User.java
@@ -0,0 +1,29 @@
+package com.smrt.smartdiagnostics.Users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long userId;
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String surname;
+    @Column(unique = true)
+    private String username; // if user uses this instead of name/surname
+    @Column(unique = true)
+    private String password;
+}
