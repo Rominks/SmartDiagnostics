604c33f8089cf009b8d951f4f009b958644b51ad
Rominks <galian2002@gmail.com>
2024-03-18 10:46:06 +0200

    Changed UserRepository getUser method params

--- src/main/java/com/smrt/smartdiagnostics/Services/UserService.java
+++ src/main/java/com/smrt/smartdiagnostics/Services/UserService.java
@@ -38,4 +38,14 @@ public class UserService {
     public void updateUser(User user) {
         userRepository.saveAndFlush(user);
     }
+    public User getUserByCredentials(User user) {
+        Optional<User> result;
+        if (user.getUsername() != null) {
+            result = userRepository.getUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+            return result.isPresent() ? result.get() : null;
+        } else {
+            result = userRepository.getUserByEmailAndPassword(user.getEmail(), user.getPassword());
+            return result.isPresent() ? result.get() : null;
+        }
+    }
 }
